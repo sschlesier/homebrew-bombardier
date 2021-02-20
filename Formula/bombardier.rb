f6db3acc392df1d51f99999236487ddff77088aa
Scott S <scott@schlesier.ca>
2021-02-20 09:57:37 -0800

    tighten up formula

--- Formula/bombardier.rb
+++ Formula/bombardier.rb
@@ -1,11 +1,7 @@
-# Documentation: https://docs.brew.sh/Formula-Cookbook
-#                https://rubydoc.brew.sh/Formula
-# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
 class Bombardier < Formula
-  desc "an HTTP(S) benchmarking tool"
-  homepage ""
+  desc "HTTP(S) benchmarking tool"
+  homepage "https://github.com/codesenberg/bombardier"
   url "https://github.com/codesenberg/bombardier/archive/v1.2.5.tar.gz"
-  version "1.2.5"
   sha256 "4efeac699d0c794ed22fe05b4de5c43fa41f98816682521421d538e076290ed6"
   license "MIT"
 
@@ -19,15 +15,6 @@ class Bombardier < Formula
   end
 
   test do
-    # `test do` will create, run in and delete a temporary directory.
-    #
-    # This test will fail and we won't accept that! For Homebrew/homebrew-core
-    # this will need to be a test that verifies the functionality of the
-    # software. Run the test with `brew test bombardier`. Options passed
-    # to `brew install` such as `--HEAD` also need to be provided to `brew test`.
-    #
-    # The installed folder is not in the path, so use the entire path to any
-    # executables being tested: `system "#{bin}/program", "do", "something"`.
     system "false"
   end
 end
