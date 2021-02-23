34be40a3a8773853d831785675d0ff42fb9322c9
Scott Schlesier <466684+sschlesier@users.noreply.github.com>
2021-02-23 06:06:35 +0000

    bombardier: add 1.2.5 bottle.

--- Formula/bombardier.rb
+++ Formula/bombardier.rb
@@ -5,6 +5,12 @@ class Bombardier < Formula
   sha256 "4efeac699d0c794ed22fe05b4de5c43fa41f98816682521421d538e076290ed6"
   license "MIT"
 
+  bottle do
+    root_url "https://github.com/sschlesier/homebrew-bombardier/releases/download/bombardier-1.2.5"
+    sha256 cellar: :any_skip_relocation, catalina:     "b2f33a0ffbdf8ed7807dffa6849dd994c24385947ae4008fe9cc8a80b84e9769"
+    sha256 cellar: :any_skip_relocation, x86_64_linux: "2d5583c3b01658bfcc0ce687b8b8e0f7ac8f3ddff8e6c17d2ef99460628a5d86"
+  end
+
   depends_on "go" => :build
 
   def install
